4473a8db67ad722525fc27881bd15d3a6a64d057
David Dean <ddean@ieee.org>
2017-09-11 08:14:35 +0000

    Seeing if ignore_label does anything useful

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ nvidia-docker-compose build
 nvidia-docker-compose run \
 	      -v $(pwd)/../skynet-data/data:/data \
 	      train \
-	        --sync s3://skynet-dbdean/output \
+	        --sync s3://skynet-dbdean/output/ignore_label \
 	        --iterations 30000 \
 	        --snapshot 10000
 
