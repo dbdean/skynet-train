c3a98a63683829f180a34236230178ac4526779d
David Dean <ddean@ieee.org>
2017-10-30 10:00:41 +0000

    Revert "Seeing if ignore_label does anything useful"
    This reverts commit 4473a8db67ad722525fc27881bd15d3a6a64d057.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ nvidia-docker-compose build
 nvidia-docker-compose run \
 	      -v $(pwd)/../skynet-data/data:/data \
 	      train \
-	        --sync s3://skynet-dbdean/output/ignore_label \
+	        --sync s3://skynet-dbdean/output \
 	        --iterations 30000 \
 	        --snapshot 10000
 
