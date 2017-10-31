8fe65242bb51355ba7e6c0f7e0e75c4aa3e14ebd
David Dean <ddean@ieee.org>
2017-10-31 03:46:20 +0000

    Change output folder, up iterations

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ nvidia-docker-compose build
 nvidia-docker-compose run \
 	      -v $(pwd)/../skynet-data/data:/data \
 	      train \
-	        --sync s3://skynet-dbdean/output \
-	        --iterations 30000 \
+	        --sync s3://skynet-dbdean/output/road_building \
+	        --iterations 50000 \
 	        --snapshot 10000
 
